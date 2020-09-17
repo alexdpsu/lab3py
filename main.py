a31f0985148df24e29344b9f6363da016f111113
alexdpsu <apd5560@psu.edu>
2020-09-17 18:49:57 +0000

    added period

--- main.py
+++ main.py
@@ -12,16 +12,16 @@ def sum_n(n):
     return n + sum_n(n-1)
     
 def print_n(s,n):
- if (n==0):
-   return
- else:
-   print(s)
-   print_n(s,n-1)
-   return
+  if (n==0):
+    return
+  else:
+    print(s)
+    print_n(s,n-1)
+    return
 
 def run():
   n = int(input("Enter an int: "))
-  print(f"sum is {sum_n(n)}")
+  print(f"sum is {sum_n(n)}.")
   s = input("Enter a string: ")
   print_n(s,n)
 
